6b00f84c81a7787debe95c04475f1ea9e042786c
20vineet <vinitmadlapure15@gmail.com>
2025-02-15 12:16:19 +0530

    Configured MongoDB and email settings

--- backend/bfms/src/main/java/com/bfms/BfmsApplication.java
+++ backend/bfms/src/main/java/com/bfms/BfmsApplication.java
@@ -9,22 +9,33 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
 @SpringBootApplication
 @EnableScheduling
 public class BfmsApplication {
 	Logger logger = LoggerFactory.getLogger(BfmsApplication.class);
 
 	public static void main(String[] args) {
+		
+		 Dotenv dotenv = Dotenv.load();
+
+	        // Set environment variables manually
+	        System.setProperty("spring.data.mongodb.uri", dotenv.get("MONGO_URI"));
+	        System.setProperty("spring.mail.username", dotenv.get("EMAIL_USERNAME"));
+	        System.setProperty("spring.mail.password", dotenv.get("EMAIL_PASSWORD"));
+
+
 		SpringApplication.run(BfmsApplication.class, args);
 	}
 
 	@Bean
-	public WebMvcConfigurer corsConfigurer() {
-		return new WebMvcConfigurer() {
-			@Override
-			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/**").allowedOrigins("*");
-			}
-		};
-	}
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedOrigins("*");
+            }
+        };
+    }
 }
